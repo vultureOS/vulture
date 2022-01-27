d54ba164ba0f1d787109917f0065d3c336440742
Krisna Pranav <krisna.pranav@gmail.com>
2022-01-27 14:21:20 +0530

    kernel: sys[update system functionalities]

--- kernel/sys/sys.go
+++ kernel/sys/sys.go
@@ -1,5 +1,7 @@
 package sys
 
+import "unsafe"
+
 const PtrSize = 4 << (^uintptr(0) >> 64)
 const PageSize = 4 << 10
 
@@ -20,3 +22,21 @@ func Hlt()
 func Cr2() uintptr
 
 func Flags() uintptr
+
+func unsafeBuffer(p uintptr, n int) []byte {
+	return (*[1 << 30]byte)(unsafe.Pointer(p))[:n]
+}
+
+func memClr(p uintptr, n int) {
+	s := (*(*[1 << 30]byte)(unsafe.Pointer(p)))[:n]
+
+	for i := range s {
+		s[i] = 0
+	}
+}
+
+func fxSave(addr uintptr)
+
+func setAx(val uintptr)
+
+func CS() uintptr
