6d89dc3236d707894367449a3c62f4728bb1e9be
Krisna Pranav <krisna.pranav@gmail.com>
2022-01-27 18:29:39 +0530

    drivers: clock
    clock functionality

--- drivers/clock/clock.go
+++ drivers/clock/clock.go
@@ -1,6 +1,16 @@
+/*
+ * Copyright (c) 2022, Krisna Pranav
+ *
+ * SPDX-License-Identifier: GPL-3.0 License
+ */
+
 package clock
 
-import "github.com/vultureOS/vulture/kernel/sys"
+import (
+	"time"
+
+	"github.com/vultureOS/vulture/kernel/sys"
+)
 
 type CmosTime struct {
 	Second int
@@ -22,19 +32,28 @@ func ReadCmosTime() CmosTime {
 	return t
 }
 
-func readCmosTime() int {
+func (c *CmosTime) Time() time.Time {
+	return time.Date(c.Year, time.Month(c.Month), c.Day, c.Hour, c.Minute, c.Second, 0, time.UTC)
+}
 
+func readCmosTime(t *CmosTime) {
+	t.Year = bcdDecode(readCmosReg(0x09)) + bcdDecode(readCmosReg(0x32))*100
+	t.Month = bcdDecode(readCmosReg(0x08))
+	t.Day = bcdDecode(readCmosReg(0x07))
+	t.Hour = bcdDecode(readCmosReg(0x04))
+	t.Minute = bcdDecode(readCmosReg(0x02))
+	t.Second = bcdDecode(readCmosReg(0x00))
 }
 
 func readCmosSecond() int {
 	return readCmosReg(0x00)
 }
 
+func bcdDecode(v int) int {
+	return v&0x0F + v/16*10
+}
+
 func readCmosReg(reg uint16) int {
 	sys.Outb(0x70, 0x80|byte(reg))
 	return int(sys.Inb(0x71))
-}
-
-func bcdDecode(v int) int {
-	return v&0x0F + v/16*10
 }
