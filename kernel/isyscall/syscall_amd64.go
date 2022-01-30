502e1944fa31676eef72affc78d64f919d48094b
Krisna Pranav <krisna.pranav@gmail.com>
2022-01-30 16:03:22 +0530

    kernel: isyscall syscall amd64

--- kernel/isyscall/syscall_amd64.go
+++ kernel/isyscall/syscall_amd64.go
@@ -1,5 +1,7 @@
 package isyscall
 
+import "unsafe"
+
 type trapFrame struct {
 	AX, BX, CX, DX    uintptr
 	BP, SI, DI, R8    uintptr
@@ -9,4 +11,41 @@ type trapFrame struct {
 	Trapno, Err uintptr
 
 	IP, CS, FLAGS, SP, SS uintptr
+}
+
+func NewRequest(tf uintptr) Request {
+	return Request{
+		tf: (*trapFrame)(unsafe.Pointer(tf)),
+	}
+}
+
+func (t *trapFrame) NO() uintptr {
+	return t.AX
+}
+
+func (t *trapFrame) Arg(n int) uintptr {
+	switch n {
+	case 0:
+		return t.DI
+	case 1:
+		return t.SI
+	case 2:
+		return t.DX
+	case 3:
+		return t.R10
+	case 4:
+		return t.R8
+	case 5:
+		return t.R9
+	default:
+		return 0
+	}
+}
+
+func (t *trapFrame) SetRet(v uintptr) {
+	t.AX = v
+}
+
+func (t *trapFrame) Ret() uintptr {
+	return t.AX
 }
