03f63abe9c1344fac70b38ded73896c880da9194
Krisna Pranav <krisna.pranav@gmail.com>
2022-01-30 16:15:43 +0530

    kernel: isyscall

--- kernel/isyscall/isyscall.go
+++ kernel/isyscall/isyscall.go
@@ -18,3 +18,19 @@ type Request struct {
 	tf   *trapFrame
 	Lock uintptr
 }
+
+func (r *Request) NO() uintptr {
+	return r.tf.NO()
+}
+
+func (r *Request) Arg(n int) uintptr {
+	return r.tf.Arg(n)
+}
+
+func (r *Request) SetRet(v uintptr) {
+	return r.tf.Ret(v)
+}
+
+func (r *Request) Ret() uintptr {
+	return r.tf.Ret()
+}
