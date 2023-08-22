80f9443db2bc399682d773e7a145eefecb129bec
krishpranav <krisna.pranav@gmail.com>
2023-08-22 22:08:30 +0530

    kernel: misc[wav]

---
+++ Kernel/Misc/WAV.cs
@@ -0,0 +1,10 @@
+/**
+ * @file WAV.cs
+ * @author Krisna Pranav
+ * @brief WAV
+ * @version 1.0 
+ * @date 2023-08-22
+ *
+ * @copyright Copyright (c) 2022 - 2023 pranaOS Developers, Krisna Pranav
+ *
+ */
