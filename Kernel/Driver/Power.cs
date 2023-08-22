963909cdaa2f8afdb23fcd3494ffce42d7db7573
krishpranav <krisna.pranav@gmail.com>
2023-08-22 21:59:00 +0530

    kernel: driver[power]

--- Kernel/Driver/Power.cs
+++ Kernel/Driver/Power.cs
@@ -9,16 +9,23 @@
  *
  */
 
-namespace Vulture.Driver
-{   
+namespace MOOS.Driver
+{
     public static class Power
-    {
+    {   
+        /// <summary>
+        ///  reboot functionalitiy sends out ps2 
+        /// </summary>
         public static void Reboot()
         {
-            while ((Native.In8(0x64) ) != 0);
-            Native.Out8(0x64);
+            while ((Native.In8(0x64) & 0x02) != 0) ;
+            Native.Out8(0x64, 0xFE);
+            Native.Hlt();
         }
 
+        /// <summary>
+        /// shutdown
+        /// </summary>
         public static void Shutdown()
         {
             ACPI.Shutdown();
