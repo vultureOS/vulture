d6a087faacfe2a481d950ca6301039e10b4e1fcc
krishpranav <krisna.pranav@gmail.com>
2023-08-22 22:08:21 +0530

    kernel: driver[acpi] g

--- Kernel/Driver/ACPI.cs
+++ Kernel/Driver/ACPI.cs
@@ -1,3 +1,14 @@
+/**
+ * @file Acpi.cs
+ * @author Krisna Pranav
+ * @brief ACPI 
+ * @version 1.0 
+ * @date 2023-08-22
+ *
+ * @copyright Copyright (c) 2022 - 2023 pranaOS Developers, Krisna Pranav
+ *
+ */
+
 using System.Collections.Generic;
 using System.Runtime.InteropServices;
 
