895d1d2f5e1653928fe86d266dafb24f626e47fc
krishpranav <krisna.pranav@gmail.com>
2023-08-23 19:12:17 +0530

    kernel: misc[png]

--- Kernel/Misc/PNG.cs
+++ Kernel/Misc/PNG.cs
@@ -24,6 +24,31 @@ namespace Vulture.Misc
             LCT_PALETTE = 3
         }
 
+        public PNG(byte[] file, LodePNGColorType type = LodePNGColorType.LCT_RGB, uint bitDept = 8)
+        {
+            lock (this)
+            {
+                fixed (byte* p = file)
+                {
+                    lodepng_decode_memory(out uint* _out, out uint w, out uint h, p, file.Length, type, bitDepth);
+
+                    if (_out == null) Panic.Error("lodepng error");
+
+                    RawData = new int[w * h];
+
+                    fixed (int* pdata = RawData)
+                    {
+                        for (int x = 0; x < w; x++)
+                        {
+                            for (int y = 0; y < h; y++)
+                            {
+                                RawData[y * w + x] = (int)((_out[y*w+x]));
+                            }
+                        }
+                    }
+                }
+            }
+        }
 
         [DllImport("*")]
         public static extern void lodepng_decode_memory(out uint* _out, out uint w, out uint h, byte* _in, int insize, LodePNGColorType colortype, uint bitdepth);
