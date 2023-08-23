b2bd1555a4bcc8c66a051647e10d38d6935cb19e
krishpranav <krisna.pranav@gmail.com>
2023-08-23 18:19:35 +0530

    kernel: misc[wav]

--- Kernel/Misc/WAV.cs
+++ Kernel/Misc/WAV.cs
@@ -17,24 +17,42 @@ namespace Vulture.Misc
     public static unsafe class WAV
     {
         [StructLayout(LayoutKind.Sequential, Pack = 1)]
-        public struct Header 
+        public struct Header
         {
             public uint ChunkID;
+            public uint ChunkSize;
+            public uint Format;
+            public uint Subchunk1ID;
+            public uint Subchunk1Size;
+            public ushort AudioFormat;
+            public ushort NumChannels;
+            public uint SampleRate;
+            public uint ByteRate;
+            public ushort BlockAlign;
+            public ushort BitsPerSample;
+            public uint Subchunk2ID;
+            public uint Subchunk2Size;
         }
 
-        public static void Decode(byte[] WAV, out byte[] PCM, out Header header)
+        public static void Decode(byte[] WAV, out byte[] PCM,out Header header)
         {
             fixed (byte* PWAV = WAV)
             {
                 Header* hdr = (Header*)PWAV;
 
-                if (hdr->AudioFormat != 1)
+                if(hdr->AudioFormat != 1) 
                 {
                     PCM = null;
                     header = default;
                     return;
                 }
+
                 PCM = new byte[hdr->Subchunk2Size];
+
+                fixed (byte* PPCM = PCM)
+                {
+                    Native.Movsb(PPCM, PWAV + sizeof(Header), (ulong)PCM.Length);
+                }
 
                 header = *hdr;
             }
