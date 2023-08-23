d37b669b23dfdbea7f28977fea47c671bd5cc00b
krishpranav <krisna.pranav@gmail.com>
2023-08-23 21:51:55 +0530

    kernel: misc[pagetable]

--- Kernel/Misc/PageTable.cs
+++ Kernel/Misc/PageTable.cs
@@ -11,7 +11,7 @@ namespace Vulture
 
         public static ulong* PML4;
 
-        internal static void Initialize()
+        internal static void Initialise()
         {
             PML4 = (ulong*)SMP.SharedPageTable;
 
@@ -19,12 +19,47 @@ namespace Vulture
 
             ulong i = 0;
 
-            for (i = (ulong)PageSize.Typical; i < 1024 * 1024; i += (ulong)PageSize.Typical)
+            for (i = (ulong)PageSize.Typical; i < 1024 * 1024 * 1024 * 4UL; i += (ulong)PageSize.Typical)
             {
                 Map(i, i, PageSize.Typical);
             }
 
             Native.WriteCR3((ulong)PML4);
+        }
+
+        public static ulong* GetPage(ulong VirtualAddress, PageSize pageSize = PageSize.Typical)
+        {
+            if((VirtualAddress % (ulong)PageSize.Typical) != 0) Panic.Error("Invalid address");
+
+            ulong pml4_entry = (VirtualAddress & ((ulong)0x1ff << 39)) >> 39;
+
+            ulong pml3_entry = (VirtualAddress & ((ulong)0x1ff << 30)) >> 30;
+            
+            ulong pml2_entry = (VirtualAddress & ((ulong)0x1ff << 21)) >> 21;
+
+            ulong pml1_entry = (VirtualAddress & ((ulong)0x1ff << 12)) >> 12;
+
+            ulong* pml3 = Next(PML4, pml4_entry);
+
+            ulong* pml2 = Next(pml3, pml3_entry);
+
+            if (pageSize == PageSize.Typical)
+            {
+                ulong* pml1 = Next(pml2, pml2_entry);
+                return &pml1[pml1_entry];
+            }
+            
+            return null;
+        }
+
+        public static void Map(ulong VirtualAddress, ulong PhysicalAddress, PageSize pageSize = PageSize.Typical)
+        {
+            if (pageSize == PageSize.Typical)
+            {
+                *GetPage(VirtualAddress, pageSize) = PhysicalAddress | 0b11;
+            }
+
+            Native.Invlpg(PhysicalAddress);
         }
 
         public static ulong* Next(ulong* Directory, ulong Entry)
@@ -33,14 +68,14 @@ namespace Vulture
 
             if (((Directory[Entry]) & 0x01) != 0)
             {
-                p = (ulong*)(Directory[Entry] & 0x000F_FFFF_FFFF_FFFF);
+                p = (ulong*)(Directory[Entry] & 0x000F_FFFF_FFFF_F000);
             }
-            else 
+            else
             {
                 p = (ulong*)Allocator.Allocate(0x1000);
                 Native.Stosb(p, 0, 0x1000);
 
-                Directory[Entry] = (((ulong)p) & 0x000F_FFFF_FFFF_FFFF);
+                Directory[Entry] = (((ulong)p) & 0x000F_FFFF_FFFF_F000) | 0b11;
             }
 
             return p;
