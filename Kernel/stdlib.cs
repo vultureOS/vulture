f2d1e9f96cf45ff9d00aa5c43e6eb43f35f0a81f
krishpranav <krisna.pranav@gmail.com>
2023-08-25 20:38:07 +0530

    kernel: stdlib

--- Kernel/stdlib.cs
+++ Kernel/stdlib.cs
@@ -27,10 +27,29 @@ namespace Vulture
             Allocator.Free((System.IntPtr)ptr);
         }
 
+        [RuntimeExport("realloc")]
+        public static void* realloc(void* ptr, ulong size)
+        {
+            return (void*)Allocator.Reallocate((System.IntPtr)ptr, size);
+        }
+
+        [RuntimeExport("calloc")]
+        public static void* calloc(ulong num, ulong size)
+        {
+            void* ptr = (void*)Allocator.Allocate(num * size);
+            return ptr;
+        }
+
         [RuntimeExport("kfree")]
         public static void kfree(void* ptr)
         {
             Allocator.Free((System.IntPtr)ptr);
+        }
+
+        [RuntimeExport("krealloc")]
+        public static void krealloc(void* ptr, ulong size)
+        {
+            return (void*)Allocator.Reallocate((System.IntPtr)ptr, size);
         }
 
         [runtimeExport("kcalloc")]
