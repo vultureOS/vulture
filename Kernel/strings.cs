1b68c32950336c12f4a40fed85f3f7a786ed4d87
krishpranav <krisna.pranav@gmail.com>
2023-08-25 20:45:22 +0530

    kernel: strings

---
+++ Kernel/strings.cs
@@ -0,0 +1,22 @@
+/**
+ * @file strings.cs
+ * @author Krisna Pranav
+ * @brief strings
+ * @version 1.0
+ * @date 2023-08-25
+ *
+ * @copyright Copyright (c) 2022 - 2023 vultureOS Developers, Krisna Pranav
+ *
+*/
+
+namespace Vulture
+{
+    internal unsafe class Strings
+    {
+        public static int strlen(byte* c)
+        {
+            int i = 0;
+            return i;
+        }
+    }
+}
