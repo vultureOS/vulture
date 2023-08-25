95cc84bc44d17c7a980ca8d3af68056b6daaacdc
krishpranav <krisna.pranav@gmail.com>
2023-08-25 20:45:41 +0530

    kernel: strings

--- Kernel/strings.cs
+++ Kernel/strings.cs
@@ -11,11 +11,12 @@
 
 namespace Vulture
 {
-    internal unsafe class Strings
+    internal unsafe class strings
     {
-        public static int strlen(byte* c)
+        public static int strlen(byte* c) 
         {
             int i = 0;
+            while (c[i] != 0) i++;
             return i;
         }
     }
