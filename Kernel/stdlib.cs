c2009ff73fc8508d0b6eec3d0f8d0ae2e1f969e2
krishpranav <krisna.pranav@gmail.com>
2023-08-25 20:38:24 +0530

    kernel: stdblib

--- Kernel/stdlib.cs
+++ Kernel/stdlib.cs
@@ -16,28 +16,36 @@ namespace Vulture
     public static unsafe class stdlib
     {
         [RuntimeExport("malloc")]
-        public static void* malloc(ulong size)
+        public static void* malloc(ulong size) 
         {
             return (void*)Allocator.Allocate(size);
         }
 
         [RuntimeExport("free")]
-        public static void free(void* ptr)
+        public static void free(void* ptr) 
         {
             Allocator.Free((System.IntPtr)ptr);
         }
 
         [RuntimeExport("realloc")]
-        public static void* realloc(void* ptr, ulong size)
+        public static void* realloc(void* ptr, ulong size) 
         {
             return (void*)Allocator.Reallocate((System.IntPtr)ptr, size);
         }
 
+
         [RuntimeExport("calloc")]
-        public static void* calloc(ulong num, ulong size)
+        public static void* calloc(ulong num,ulong size)
         {
             void* ptr = (void*)Allocator.Allocate(num * size);
+            Native.Stosb(ptr, 0, num * size);
             return ptr;
+        }
+
+        [RuntimeExport("kmalloc")]
+        public static void* kmalloc(ulong size)
+        {
+            return (void*)Allocator.Allocate(size);
         }
 
         [RuntimeExport("kfree")]
@@ -47,17 +55,18 @@ namespace Vulture
         }
 
         [RuntimeExport("krealloc")]
-        public static void krealloc(void* ptr, ulong size)
+        public static void* krealloc(void* ptr, ulong size)
         {
             return (void*)Allocator.Reallocate((System.IntPtr)ptr, size);
         }
 
-        [runtimeExport("kcalloc")]
+
+        [RuntimeExport("kcalloc")]
         public static void* kcalloc(ulong num, ulong size)
         {
-           void* ptr = (void*)Allocator.Allocate(num * size);
-           Native.Stosb(ptr, 0, num * size);
-           return ptr; 
+            void* ptr = (void*)Allocator.Allocate(num * size);
+            Native.Stosb(ptr, 0, num * size);
+            return ptr;
         }
     }
 }
