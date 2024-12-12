a1c2b6f89148afd2af9902b16d02bc05b359e476
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2024-12-12 13:09:16 +0530

    byte_endian: macro_rules

--- byte_endian/src/lib.rs
+++ byte_endian/src/lib.rs
@@ -1,14 +1,58 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+/**
+ * @file lib.rs
+ * @author Krisna Pranav
+ * @brief pty
+ * @version 3.0
+ * @date 2024-09-28
+ *
+ * @copyright Copyright (c) 2022-2024 vultureOS Developers, Krisna Pranav
+ *
+ */
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+#![no_std]
+#![warn(clippy::pedantic)]
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+macro_rules! impl_traits {
+    ($($endian_type:ident),+) => {
+        $(
+            impl Endian<$endian_type> for $endian_type {
+                fn to_be(&self) -> $endian_type {
+                    <$endian_type>::to_be(*self)
+                }
+
+                fn to_le(&self) -> $endian_type {
+                    <$endian_type>::to_le(*self)
+                }
+
+                fn from_be(value: $endian_type) -> $endian_type {
+                    <$endian_type>::from_be(value)
+                }
+
+                fn from_le(value: $endian_type) -> $endian_type {
+                    <$endian_type>::from_le(value)
+                }
+            }
+        )+
+
+        impl_traits!(@make_impl $($endian_type),+ => LittleEndian);
+        impl_traits!(@make_impl $($endian_type),+ => BigEndian);
+    };
+
+    (@make_impl $($endian_type:ident),+ => $type:ident) => {
+        impl<T: Endian<T>> From<T> for $type<T> {
+            #[inline]
+            fn from(value: T) -> Self {
+                Self::new(value)
+            }
+        }
+
+        $(
+            impl From<$type<$endian_type>> for $endian_type {
+                #[inline]
+                fn from(value: $type<$endian_type>) -> Self {
+                    value.to_native()
+                }
+            }
+        )*
+    };
 }
