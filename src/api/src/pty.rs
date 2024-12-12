06ca3ebb6bb10b7ab720b4dd47dfcd071745d4ee
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2024-12-12 12:58:38 +0530

    pty

--- src/api/src/pty.rs
+++ src/api/src/pty.rs
@@ -4,11 +4,10 @@
  * @brief pty
  * @version 3.0
  * @date 2024-09-28
- * 
+ *
  * @copyright Copyright (c) 2022-2024 vultureOS Developers, Krisna Pranav
- * 
+ *
  */
-
 use crate::ioctl;
 
 pub const TIOCGPTN: usize = ioctl::ior::<u32>('T' as usize, 0x30);
